7c7a9dfa6a4088a604730042fd30e1c5c476c5be
Sabrina Helena Ferreira <101990175+sabrinahelena@users.noreply.github.com>
2023-02-12 11:00:54 -0300

    Update Main.java

--- Lab01-ContarMaiusculos/src/Main.java
+++ Lab01-ContarMaiusculos/src/Main.java
@@ -2,43 +2,31 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static boolean isMaiuscula (char c){
-        return (c >= 'A' && c <= 'Z');
-    }
+    public static void main(String[] args) {
 
-    public static boolean isFim(String s){
-        return (s.length() == 3 && s.charAt(0) == 'F' && s.charAt(1) == 'I' && s.charAt(2) == 'M');
-    }
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> strings = new ArrayList<>();
 
-    /*
-     *
-     */
-    public static int contarLetrasMaiusculas (String s, int pos){
-        int resp = 0;
-        if(pos < s.length()){
-            if(isMaiuscula(s.charAt(pos)) == true){
-                resp = 1 + contarLetrasMaiusculas (s, pos + 1);
-            } else {
-                resp = contarLetrasMaiusculas (s, pos + 1);
+        System.out.println("Digite as palavras ou textos para a contagem de maiusculas. (digite FIM para encerrar):");
+        String str = scanner.nextLine();
+        while (!str.equals("FIM")) { //enquanto a string digitada pela pessoa for diferente da palavra FIM
+            strings.add(str); //Adiciona na arraylist a palavra/texto digitado
+            str = scanner.nextLine();
+        }//agora, se for FIM, nao vai adicionar o que a pessoa digitou na lista
+
+        contarLetrasMaiusculas(strings); // chama o método para a lista
+    }
+    //I = Palavra - J = Letra
+    public static void contarLetrasMaiusculas(ArrayList<String> strings) {
+        for (int i = 0; i < strings.size(); i++) { //acesso cada string dentro da lista
+            int contador = 0;
+            for (int j = 0; j < strings.get(i).length(); j++) { //aqui, acesso cada letra, pegando o  tamanho de cada string passada para percorrer
+                char c = strings.get(i).charAt(j); //atribuo cada letra da string I na posicao J
+                if (Character.isUpperCase(c)) {//verifico se é maiuscula
+                    contador++;
+                }
             }
-        }
-        return resp;
-    }
-
-    public static void main (String[] args){
-        String[] entrada = new String[1000];
-        int numEntrada = 1;
-        entrada[0] = "cOmIdA";
-
-        //Leitura da entrada padrao
-        //  do {
-        //  entrada[numEntrada] = MyIO.readLine();
-        //} while (isFim(entrada[numEntrada++]) == false);
-        //numEntrada--;   //Desconsiderar ultima linha contendo a palavra FIM
-
-        //Para cada linha de entrada, gerando uma de saida contendo o numero de letras maiusculas da entrada
-        for(int i = 0; i < numEntrada; i++){
-            System.out.println(contarLetrasMaiusculas(entrada[i],0));
+            System.out.println("A string " + (i + 1) + " tem " + contador + " letras maiúsculas.");
         }
     }
 }
