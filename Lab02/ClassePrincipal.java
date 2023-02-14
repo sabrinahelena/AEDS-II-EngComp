dad12fb2410631b3ae886df114e1860e1b029b98
Sabrina Helena Ferreira <101990175+sabrinahelena@users.noreply.github.com>
2023-02-13 22:09:28 -0300

    Update ClassePrincipal.java

--- Lab02/ClassePrincipal.java
+++ Lab02/ClassePrincipal.java
@@ -1,63 +1,17 @@
 package Pratica02;
-import java.util.Arrays;
 
-public class Dados {
+public class ClassePrincipal {
 
-	private double Array[];
-	private int N; 
-	
-	public Dados() {
-		Array = new double[10];
-		N = Array.length;
-	}
-	
-	public Dados(double[] array) {
-		Array = array;
-		N = array.length;
-	}
-	
-	public void ImprimirArray() {
-		for(int i = 0; i<N; i++) {
-			System.out.println("Posicao: " + i + ": " + Array[i]);
-		}
-	}
-	
-	public double CalcularMedia() {
-		double sum = 0;
-		for(int i = 0; i<N; i++) {
-			sum += Array[i];
-		}
-		return sum/N;
-	}
-	
-	public void Ordenar() {
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if (Array[i] < Array[j]) {
-					double temp = Array[i];
-					Array[i] = Array[j];
-					Array[j] = temp;
-				}
-			}
-		}
-		//Arrays.sort(Array);
-	}
-	
+	public static void main(String[] args) {
+		double[] V = {1,2,4,6,2,3};
+		 Dados D1 = new Dados(V);
+		 D1.ImprimirArray();
+		 System.out.println("A media e': " + D1.CalcularMedia());
+		 System.out.println("O vetor ordenado e': ");
+		 D1.Ordenar();
+		 D1.ImprimirArray();
+		 System.out.println("A mediana e': " + D1.CalcularMediana());
 
-	public double CalcularMediana() {
-		Ordenar();
-		double median;
-		int esquerda = 0, direita = 0;
-
-		if(N % 2 != 0){
-			median = Array[N/2];
-		} else {
-			esquerda = ( N/2) - 1;
-			direita = esquerda + 1;
-			median = (Array[esquerda] + Array[direita]) / 2;
-		}
-		return median;
 	}
 
 }
-
