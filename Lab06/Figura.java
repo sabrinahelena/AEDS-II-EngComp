bb68ad690700079f1caf558c69c7573cfa5192ce
Sabrina Helena Ferreira <101990175+sabrinahelena@users.noreply.github.com>
2023-03-20 22:20:37 -0300

    Create Figura.java

---
+++ Lab06/Figura.java
@@ -0,0 +1,25 @@
+package Atividade;
+
+public abstract class Figura{
+	public int x0;
+	public int y0;
+	
+	public Figura() {
+		x0 = 0; y0 = 0;
+	}
+	
+	
+	public Figura(int x, int y) {
+		x0 = x; y0 = y;
+	}
+	
+	public String toString() {
+		return "Figura("+x0+" : " + y0+")";
+	}
+	
+	public abstract double GetArea ();
+	public abstract double GetPerimetro();
+}
+
+
+
