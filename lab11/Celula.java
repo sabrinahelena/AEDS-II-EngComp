51331a7c3e2d8f55a7a58d4b3050c53445f52e0f
Sabrina Helena Ferreira <101990175+sabrinahelena@users.noreply.github.com>
2023-05-08 21:55:32 -0300

    Create Celula.java

---
+++ lab11/Celula.java
@@ -0,0 +1,14 @@
+package pratica12;
+
+public class Celula {
+	int elemento;
+	Celula esq, dir, sup, inf;
+	
+	public Celula() {
+		this.elemento = 0;
+	}
+	
+	public Celula(int x) {
+		this.elemento = x;
+	}
+}
